a499fd65fbd19580a64cfcf68c1381aec74646b4
Fabioggk <81123748+ingratal@users.noreply.github.com>
2024-10-22 20:44:24 -0300

    make it better

---
+++ server.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const Web3 = require('web3');
+
+
+// Define the port to run the server
+const PORT = process.env.PORT || 4200;
+
+// Serve static files (your Webflow export)
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Basic route to serve index.html from the public directory
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html')); // Updated path
+});
+
+
+// Example API route (VERCEL)
+module.exports = (req, res) => {
+  res.status(200).json({ message: 'Server is running!' });
+};
+
+
+// Start the server
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
+
